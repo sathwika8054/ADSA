999704ba66fae07ae9eee1931e6c3c9b0dc86e55
Sathwika8054 <sathwika638@gmail.com>
2026-08-21 09:33:36 +0530

    Organize sliding window assignments

---
+++ Assignments/M01_Sliding_Window_&_Two_Pointers/AST02/task.py
@@ -0,0 +1,24 @@
+def Check_Palindrome(n: int, s: str) -> bool:
+    left = 0
+    right = n - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            # Try deleting the left character
+            left_check = s[left + 1:right + 1]
+
+            # Try deleting the right character
+            right_check = s[left:right]
+
+            return left_check == left_check[::-1] or right_check == right_check[::-1]
+
+        left += 1
+        right -= 1
+
+    return True
+
+
+if __name__ == '__main__':
+    n = int(input())
+    s = input()
+    print(Check_Palindrome(n, s))
--- practice/M02_2D_Arrays_&_Matrix_Logic/S05/Search_in_Sorted_matrix.py
+++ practice/M02_2D_Arrays_&_Matrix_Logic/S05/Search_in_Sorted_matrix.py
@@ -40,3 +40,17 @@ print(searchMatrixs(matrix,target))
 
 
 #240
+def SearchMatrix(matrix: List[List[int]], target: int) -> bool:
+        m,n=len(matrix),len(matrix[0])
+        r,c=0,n-1
+        while r<m and c>=0:
+            if target == matrix[r][c]:
+                return True
+            elif target < matrix[r][c]:
+                c-=1 
+            else:
+                r+=1 
+        return False
+matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+target = 5
+print(SearchMatrix(matrix,target))
